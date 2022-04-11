2dd7bcf4de5ac861375471609f9944072da5c9ef
khenchi <khalilhenchi@gmail.com>
2022-04-11 19:26:16 +0200

    Update configurations

--- FastApi.py
+++ FastApi.py
@@ -49,4 +49,4 @@ def predict_score(userid):
 # 4. Run the API with uvicorn
 #    Will run on http://127.0.0.1:8000
 if __name__ == '__main__':
-    uvicorn.run(app, host='127.0.0.1', port=8000)
+    uvicorn.run(app)
--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: uvicorn app.FastApi:app --host=0.0.0.0 --port=${PORT:-5000}
+web: uvicorn FastApi:app --log-file -
